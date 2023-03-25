4dd603a6120751da3109b450b49a23dfe81f82dc
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-25 21:49:45 +0530

    implement new POST endpoint for add new users

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
+
+
+@app.route('/api/users/add', methods=['POST'])
+def api_add_user():
+    user = request.get_json()
+    print(user['name'], user['age'])
+    return jsonify("ok")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
