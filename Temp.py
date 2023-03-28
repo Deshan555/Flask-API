fb7e946075f9fe2ce635d4217bf7a972cb7560cb
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-28 17:42:29 +0530

    write functions for get Temperature, HeatIndex, GasValue

---
+++ Temp.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+import Config
+
+
+def temperature(date):
+    sql_connection = mysql.connector.connect(host=Config.HOST_NAME, user=Config.USER_NAME, password=Config.PASSWORD,
+                                             database=Config.DATABASE_NAME)
+
+    connection = sql_connection.cursor()
+
+    SQL = "SELECT Record_Time, Temperature FROM sensor_box_01 WHERE Record_Date = '" + date + "' LIMIT 10;"
+
+    connection.execute(SQL)
+
+    result = connection.fetchall()
+
+    data_dict: dict = {}
+
+    for x in result:
+        data_dict[x[0]] = x[1]
+
+    sql_connection.close()
+
+    return data_dict
+
+
+
