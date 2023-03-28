31d5d78cce4523e9e443c0984e07b01dd7e2331d
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-28 16:52:21 +0530

    add new end point for return 10 humidity data based on given date

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, jsonify
 
+import Box_01
+
+import Box_2
+
+import humidity
+
 app = Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
@@ -12,6 +18,23 @@ def api_add_user():
     user = request.get_json()
     print(user['name'], user['age'])
     return jsonify("ok")
+
+
+@app.route('/v01/device/box_01/', methods=['GET'])
+def get_Box_01():
+    return jsonify(Box_01.sensor_Box1())
+
+
+@app.route('/v01/device/box_02/', methods=['GET'])
+def get_Box_02():
+    return jsonify(Box_2.sensor_Box2())
+
+
+@app.route('/v01/device/humidity/', methods=['POST'])
+def humidity_get():
+    date = request.get_json()
+    return_data = humidity.humidity(date['date'])
+    return jsonify(return_data)
 
 
 if __name__ == '__main__':
